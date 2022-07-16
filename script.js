92abf7513889575a346f74387afb85ac6bba7334
muhajirahmadh <95091883+muhajirahmadh@users.noreply.github.com>
2022-07-16 15:22:44 +0530

    change

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 function clicke(){
     document.getElementById("1").innerHTML = Date();
 }
+
+function mouse(){
+    {
+		alert("This is JavaTpoint");
+	}
+}
+
+function focuse(){
+    {
+		document.getElementById("2").style.background =" grey";
+	}
+}
+function kee(){
+    {
+		document.getElementById("3");
+        alert("Key Pressed")
+	}
+}
+function uppercase(){
+    {
+		var x = document.getElementById("4");
+        x.value = x.value.toUpperCase();
+	}
+}
+
+function preferedBrowser(){
+    prefer = document.forms[0].browsers.value;
+    alert("you are choose this browser" +prefer);
+}
