9c8842437213121bb8e2efc24dd0e1b2c2c4859c
muhajirahmadh <95091883+muhajirahmadh@users.noreply.github.com>
2022-07-18 14:34:43 +0530

    change

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-function clicke(){
-    document.getElementById("1").innerHTML = Date();
+// choose browswer
+document.getElementById("btn").addEventListener("click",preferedBrowser);
+function preferedBrowser(){
+    prefer = document.forms[0].browsers.value;
+    alert("you are choose this browser" +prefer);
 }
 
+
+//click
+document.getElementById("btn").addEventListener("click",clicke);
+
+
+function clicke(){
+    document.getElementById("para").innerHTML = Date();
+}
+
+// mouse event
+document.getElementById("btn").addEventListener("click",mouse);
 function mouse(){
     {
 		alert("This is JavaTpoint");
 	}
 }
 
+// focus event
+document.getElementById("btn").addEventListener("click",focuse);
 function focuse(){
     {
 		document.getElementById("2").style.background =" grey";
 	}
 }
+
+//keyboard
+document.getElementById("btn").addEventListener("click",kee);
 function kee(){
     {
 		document.getElementById("3");
         alert("Key Pressed")
 	}
 }
+
+//uppercase
+document.getElementById("btn").addEventListener("click",uppercase);
 function uppercase(){
     {
 		var x = document.getElementById("4");
@@ -26,7 +48,3 @@ function uppercase(){
 	}
 }
 
-function preferedBrowser(){
-    prefer = document.forms[0].browsers.value;
-    alert("you are choose this browser" +prefer);
-}
